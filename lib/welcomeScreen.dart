af8999db3936aa676ece7e2f513071a83f509cf8
exam <exam@Aptechnk1.edu>
2024-09-26 17:47:02 +0500

    muskan

--- lib/welcomeScreen.dart
+++ lib/welcomeScreen.dart
@@ -7,12 +7,25 @@ class welcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        //  color: Colors.black,
         height: 250,
         width: 300,
-        child: const Text(
-          "Muskan",
-          style: TextStyle(color: Colors.white),
+        decoration: const BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50)
+                )),
+
+        child: const Center(
+          child: Text(
+            "Muskan \n Arshad",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w100,
+                letterSpacing: 4),
+          ),
         ),
       ),
     );
