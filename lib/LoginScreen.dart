a22bda3acfba97d1d21adc0e039967be4d278ddb
exam <exam@Aptechnk1.edu>
2024-09-28 18:59:13 +0500

    messageeee

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:project/customButton.dart';
+import 'package:project/customTextField.dart';
 
 class Loginscreen extends StatelessWidget {
   const Loginscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    TextEditingController passwordcontroller = TextEditingController();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15),
@@ -15,16 +19,87 @@ class Loginscreen extends StatelessWidget {
               "Login",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
             ),
-            const TextField(),
+            // TextField(
+            //   controller: emailController,
+            //   decoration: const InputDecoration(
+            //     prefixIcon: Icon(
+            //       Icons.email,
+            //       color: Colors.grey,
+            //     ),
+            //     errorBorder: OutlineInputBorder(
+            //         borderSide: BorderSide(
+            //           color: Colors.red,
+            //           width: 2,
+            //         ),
+            //         borderRadius: BorderRadius.all(Radius.circular(12))),
+            //     focusedBorder: OutlineInputBorder(
+            //         borderSide: BorderSide(
+            //           color: Colors.lightGreen,
+            //           width: 2,
+            //         ),
+            //         borderRadius: BorderRadius.all(Radius.circular(12))),
+            //     border: OutlineInputBorder(
+            //         borderSide: BorderSide(
+            //           color: Colors.grey,
+            //           width: 2,
+            //         ),
+            //         borderRadius: BorderRadius.all(Radius.circular(12))),
+            //   ),
+            // ),
+
+            CustomTextField(
+              suffixIcon: Icon(color: Colors.grey, Icons.email),
+              controller: emailController,
+            ),
+
             const SizedBox(
               height: 20,
             ),
-            const TextField(),
+            CustomTextField(
+              suffixIcon: Icon(color: Colors.grey, Icons.lock),
+              ispass: true,
+              controller: passwordcontroller,
+            ),
+
             ElevatedButton(
                 onPressed: () {
-                  print("hello");
+                  print(emailController.text.trim());
                 },
                 child: Text("Login")),
+            MaterialButton(
+                onPressed: () {
+                  print("SignUp");
+                },
+                child: Text("SignUp")),
+
+            // InkWell(
+            //     onTap: () {
+            //       print(emailController.text.trim());
+            //       print(passwordcontroller.text.trim());
+            //     },
+            //     child: Container(
+            //       height: 50,
+            //       width: double.infinity,
+            //       decoration: BoxDecoration(
+            //           color: Colors.green,
+            //           borderRadius: BorderRadius.circular(20)),
+            //       child: Center(
+            //         child: Text(
+            //           "Forget Password?",
+            //           style: TextStyle(
+            //             color: Colors.white,
+            //           ),
+            //         ),
+            //       ),
+            //     ))
+
+            CustomButton(
+              onTap: () {
+                print(emailController.text.trim());
+                print(passwordcontroller.text.trim());
+              },
+              text: "LOG IN",
+            )
           ],
         ),
       ),
